20a41efaf837036acda7f79c61bbe11dfdc28251
MHeinz1996 <michael.heinzinger@gmail.com>
2022-06-05 10:50:56 -0500

    Testing output

--- runner.py
+++ runner.py
@@ -5,8 +5,5 @@ from classes.staff import Staff
 school = School('Ridgemont High') 
 
 print(school.name)
-
-# student_info = {'name': 'Diana', 'password': 'password', 'school_id' : 12345, 'age' : 17, 'role' : 'Student'}
-# Student(**student_info) # This syntax allows us to send a dict of kwargs through the class to init the instance
-# print(Student.all_students())
-# print(Staff.all_staff())
+print(school.staff)
+print(school.students)
